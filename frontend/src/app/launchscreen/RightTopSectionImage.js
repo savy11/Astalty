ad685dae986976c00c45fee2ff56c340841e8c23
Shubham Kumar <shubhamkumar.ss@smartdatainc.net>
2026-01-28 01:30:47 +0530

    Netlify Changes 2

--- frontend/src/app/launchscreen/RightTopSectionImage.js
+++ frontend/src/app/launchscreen/RightTopSectionImage.js
@@ -1,14 +1,14 @@
 "use client";
+import Image from "next/image";
 
 export default function SignupForm() {
-
   return (
-   <div className="w-full h-[800px] flex items-center justify-center bg-gray-100">
-  <img
-    src="/images/bannerhospital.svg"
-    alt="Centered"
-    className="object-contain max-w-full max-h-full"
-  />
-  </div>
+    <div className="w-full h-[800px] flex items-center justify-center bg-gray-100">
+      <Image
+        src="/images/bannerhospital.svg"
+        alt="Centered"
+        className="object-contain max-w-full max-h-full"
+      />
+    </div>
   );
 }
